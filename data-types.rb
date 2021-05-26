2db6d717981c6ac9763a46094b0887df31e74160
Sara Barros <65254818+sarashb@users.noreply.github.com>
2021-05-25 23:20:24 -0300

    Data types

---
+++ data-types.rb
@@ -0,0 +1,26 @@
+# Check whether this number is an odd number:
+integer = 68
+# your code here:
+puts integer.odd?
+
+# Display the result of a divided per b:
+a = 10
+b = 2
+# your code here:
+puts a / b
+
+# Rounding a float
+float = 7.2
+puts float.round
+
+# Use a method to write this word in capital letters:
+word = 'ruby'
+puts word.capitalize
+
+# A method to write in upper case
+word = 'ruby'
+puts word.upcase
+
+# A condition that wourld return true
+condition = 10 > 5
+puts condition
